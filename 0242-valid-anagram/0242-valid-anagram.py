191d3897b71a61efd4b8a85ba557db38043bcfc4
Afham <afhamtab@gmail.com>
2026-09-13 21:09:24 +0530

    Time: 11 ms (95.96%), Space: 12.5 MB (86.4%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,6 +1,17 @@
 class Solution(object):
     def isAnagram(self, s, t):
-        if sorted(s) == sorted(t):
-            return True
-        else:
-            return False
+        freq = {}
+        for i in s:
+            if i in freq:
+                freq[i] += 1
+            else:
+                freq[i] = 1
+
+        freq2 = {}
+        for i in t:
+            if i in freq2:
+                freq2[i] += 1
+            else:
+                freq2[i] = 1
+
+        return freq == freq2
