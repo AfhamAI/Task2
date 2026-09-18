34c84194bf3c991cde85bfe1837eda1a1e96e9ce
Afham <afhamtab@gmail.com>
2026-09-18 14:51:11 +0530

    Time: 260 ms (95.9%), Space: 0B (100%) - LeetHub

--- 0596-classes-with-at-least-5-students/0596-classes-with-at-least-5-students.sql
+++ 0596-classes-with-at-least-5-students/0596-classes-with-at-least-5-students.sql
@@ -1,6 +1,2 @@
 -- Write your PostgreSQL query statement below
-with temp as (
-    select class , count(student) from Courses group by class
-)
-
-select class from temp where count >= 5
+select class from Courses group by class having count(student) >= 5 
