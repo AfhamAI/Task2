2e0ed44ffc4ca542fc822aae849d2be8743dded3
Afham <afhamtab@gmail.com>
2026-09-22 10:44:47 +0530

    Time: 513 ms (56.76%), Space: 0B (100%) - LeetHub

--- 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
+++ 0550-game-play-analysis-iv/0550-game-play-analysis-iv.sql
@@ -2,6 +2,9 @@
 with temp as(
     select player_id , min(event_date) as firstLogin from Activity group by player_id
 )
-select round(count(*)::numeric/(SELECT COUNT(*) FROM temp),2) as fraction from Activity a 
+
+select round(count(*)::numeric/(SELECT COUNT(*) FROM temp),2) 
+as fraction 
+from Activity a 
 join temp t
 on a.event_date = firstLogin + INTERVAL '1 DAY' and a.player_id = t.player_id
