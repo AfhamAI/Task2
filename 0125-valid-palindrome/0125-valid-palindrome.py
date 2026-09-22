df9919cb1bb83598489bd1ca1e5dc0607c5ab1c7
Afham <afhamtab@gmail.com>
2026-09-22 10:32:06 +0530

    Time: 5 ms (87.18%), Space: 19.6 MB (78.55%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        y = ""
+        for i in s:
+            if i.isalnum():
+                y += i.lower()
+        return y == y[::-1]
+        
+        
+        
+        
+ 
